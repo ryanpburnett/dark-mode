e155f4be37b40364a4824e579a3352f3bfd43ee8
Ryan <ryanpburnett@yahoo.com>
2021-06-19 13:34:48 -0500

    first commit

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+let switcheroo = document.querySelector("#switcheroo");
+let boxen = document.querySelector(".boxen");
+let mode = "dark"
+
+switcheroo.addEventListener("click", function() {
+    if (mode === "dark") {
+        mode = "light";
+        boxen.setAttribute("class", "light");
+    } else {
+        mode = "dark";
+        boxen.setAttribute("class", "dark")
+    }
+})
